a717c2fa6289b343fec37a1c0b80800c6c26cc32
terryttsai <iliveinabucket@berkeley.edu>
2016-01-14 23:01:40 -0800

    added sections, changed navbar with added sticky behavior

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
+mns = "sticky_nav_scrolled";
+
+function jqUpdateSize(){
+    if( $(this).scrollTop() > $('#about').height() - 99 ) {
+        $('.sticky_nav').addClass(mns);
+    } else {
+        $('.sticky_nav').removeClass(mns);
+    }
+};
+
+$(window).resize(jqUpdateSize);     // When the browser changes size
+
+$(window).scroll(function() {
+  if( $(this).scrollTop() > $('#about').height() - 99 ) {
+    $('.sticky_nav').addClass(mns);
+  } else {
+    $('.sticky_nav').removeClass(mns);
+  }
+});
+
+$(function() {      //autoscroll script
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top - 99
+        }, 1000);
+        return false;
+      }
+    }
+  });
+});
+
 $(document).ready(function() {
     window.onload = function() {
         $('body').fadeIn(1400);
     }
-    
+    jqUpdateSize();
 });
